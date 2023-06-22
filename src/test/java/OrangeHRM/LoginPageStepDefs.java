c2aec5eb62573fcd790330f7359e7bdb58ee73fc
Thapelo-Sean <thapelomatji12@gmail.com>
2023-06-22 13:55:52 +0200

    Created LoginPageStepDefs file

---
+++ src/test/java/OrangeHRM/LoginPageStepDefs.java
@@ -0,0 +1,39 @@
+package OrangeHRM;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginPageStepDefs {
+    @Given("I have entered a valid username and password")
+    public void iHaveEnteredAValidUsernameAndPassword() {
+    }
+
+    @When("I click the login button")
+    public void iClickTheLoginButton() {
+    }
+
+    @Then("I should be logged in successfully")
+    public void iShouldBeLoggedInSuccessfully() {
+    }
+
+    @Given("I have entered invalid {string} and {string}")
+    public void iHaveEnteredInvalidAnd(String arg0, String arg1, String arg2, String arg3) {
+    }
+
+    @When("I click on the login button")
+    public void iClickOnTheLoginButton() {
+    }
+
+    @Then("I should see an error message indicating {string}")
+    public void iShouldSeeAnErrorMessageIndicating(String arg0, String arg1) {
+    }
+
+    @When("I click on the forgot your password? link")
+    public void iClickOnTheForgotYourPasswordLink() {
+    }
+
+    @Then("I should be redirected to the password reset page")
+    public void iShouldBeRedirectedToThePasswordResetPage() {
+    }
+}
