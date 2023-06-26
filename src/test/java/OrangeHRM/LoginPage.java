3bdbb6cffdd950463a07334320f261945c5c3a0b
Thapelo-Sean <thapelomatji12@gmail.com>
2023-06-26 08:59:27 +0200

    Modified LoginPage class

--- src/test/java/OrangeHRM/LoginPage.java
+++ src/test/java/OrangeHRM/LoginPage.java
@@ -1,4 +1,41 @@
 package OrangeHRM;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class LoginPage {
+    private WebDriver driver;
+
+    //Constructor
+    public LoginPage(WebDriver driver)
+    {
+        this.driver = driver;
+    }
+
+    //Locate elements
+    private By usernameInputLocator = By.name("username");
+    private By passwordInputLocator = By.name("password");
+    private By loginButtonLocator = By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button");
+
+    //Methods
+    public void setUsername(String stringUsername)
+    {
+        WebElement username = driver.findElement(usernameInputLocator);
+        username.clear();
+        username.sendKeys(stringUsername);
+    }
+
+    public void setPassword(String stringPassword)
+    {
+        WebElement password = driver.findElement(passwordInputLocator);
+        password.clear();
+        password.sendKeys(stringPassword);
+    }
+
+    public void clickLoginButton()
+    {
+        WebElement loginButton = driver.findElement(loginButtonLocator);
+        loginButton.click();
+    }
 }
