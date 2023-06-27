842b2ef461e7c3aa509b25e9ef271fedb60f1615
Thapelo-Sean <thapelomatji12@gmail.com>
2023-06-27 12:45:15 +0200

    Modified MyStepdefs class

--- src/test/java/MyStepdefs.java
+++ src/test/java/MyStepdefs.java
@@ -38,188 +38,296 @@ import java.util.concurrent.TimeUnit;
         private LoginPage loginPage;
         private DashboardPage dashboardPage;
 
+
         @Before
         public void initBrowser()
         {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-            loginPage = new LoginPage(driver);
-            dashboardPage = new DashboardPage(driver);
+            try
+            {
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                loginPage = new LoginPage(driver);
+                dashboardPage = new DashboardPage(driver);
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
         }
 
         @After
         public void tearDown()
         {
-            if (driver != null)
+            try
             {
-                driver.quit();
+                if (driver != null)
+                {
+                    driver.quit();
+                }
+            } catch (Exception e)
+            {
+                e.printStackTrace();
             }
         }
 
         @Given("User navigated to the login page")
         public void iNavigatedToTheLoginPage()
         {
-            driver.get(baseUrl);
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+            try
+            {
+                driver.get(baseUrl);
+                driver.manage().window().maximize();
+                driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
         }
 
         @When("User enters valid username and password")
         public void iEnterValidUsernameAndPassword()
         {
-            loginPage.setUsername("Admin");
-            loginPage.setPassword("admin123");
+            try
+            {
+                loginPage.setUsername("Admin");
+                loginPage.setPassword("admin123");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
         @And("User clicks the login button")
         public void iClickTheLoginButton()
         {
-            loginPage.clickLoginButton();
+            try
+            {
+                loginPage.clickLoginButton();
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
         }
 
         @Then("User should be logged in successfully")
         public void iShouldBeLoggedInSuccessfully()
         {
-            if (driver.getCurrentUrl().equalsIgnoreCase(dashboardUrl))
+            try
             {
-                Assert.assertTrue(true);
-            } else
+                if (driver.getCurrentUrl().equalsIgnoreCase(dashboardUrl))
+                {
+                    Assert.assertTrue(true);
+                } else
+                {
+                    Assert.fail();
+                }
+            } catch (Exception e)
             {
-                Assert.fail();
+                e.printStackTrace();
             }
         }
 
         @When("User enters invalid username and password")
         public void iHaveEnteredInvalidUsernameAndPassword()
         {
-            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-            loginPage.setUsername("Invalid Username");
-            loginPage.setPassword("Invalid Password");
+            try
+            {
+                driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+                loginPage.setUsername("Invalid Username");
+                loginPage.setPassword("Invalid Password");
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
         }
 
         @Then("User should get an error message")
         public void iShouldGetAnErrorMessage()
         {
-            Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/p")).isDisplayed(),true);
+            try
+            {
+                Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/p")).isDisplayed(),true);
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
         }
 
         @When("User clicks the forgot password link")
         public void iClickTheForgotPasswordLink()
         {
-            loginPage.clickForgotPassword();
+            try
+            {
+                loginPage.clickForgotPassword();
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
         }
 
         @Then("User should be navigated to the forgot Password page")
         public void iShouldBeNavigatedToTheForgotPasswordPage()
         {
-            String forgotPasswordUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/requestPasswordResetCode";
-            Assert.assertEquals(driver.getCurrentUrl(), forgotPasswordUrl);
+            try
+            {
+                String forgotPasswordUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/requestPasswordResetCode";
+                Assert.assertEquals(driver.getCurrentUrl(), forgotPasswordUrl);
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
         }
 
         @And("User enters valid username")
         public void iEnterValidUsername()
         {
-            loginPage.setUsername("Admin");
+            try
+            {
+                loginPage.setUsername("Admin");
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
         }
 
         @And("User clicks the reset password button")
         public void clickTheResetPasswordButton()
         {
-            loginPage.clickResetPassword();
+            try
+            {
+                loginPage.clickResetPassword();
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
         }
 
         @Then("Reset password link should be sent successfully")
         public void resetPasswordLinkShouldBeSentSuccessfully()
         {
-            Assert.assertEquals(driver.getCurrentUrl(),sendPasswordResetUrl);
-            Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/div/h6")).isDisplayed());
+            try
+            {
+                Assert.assertEquals(driver.getCurrentUrl(),sendPasswordResetUrl);
+                Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/div/h6")).isDisplayed());
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
         }
 
         @Given("User navigated to the dashboard page")
         public void userNavigatedToTheDashboardPage()
         {
-            driver.get(dashboardUrl);
+            try
+            {
+                driver.get(dashboardUrl);
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
         }
 
         @When("User clicks any of the navigation links")
         public void userClicksAnyOfTheNavigationLinks()
         {
-            iNavigatedToTheLoginPage();
-            iEnterValidUsernameAndPassword();
-            iClickTheLoginButton();
-            dashboardPage.clickAdminLink();
-            dashboardPage.clickDashboardLink();
+            try
+            {
+                iNavigatedToTheLoginPage();
+                iEnterValidUsernameAndPassword();
+                iClickTheLoginButton();
+                dashboardPage.clickAdminLink();
+                dashboardPage.clickDashboardLink();
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
         }
 
         @Then("User should not see a {int} error or broken links")
         public void userShouldNotSeeAErrorOrBrokenLinks(int arg0) throws IOException
         {
-            List<WebElement> links = driver.findElements(By.tagName("a"));
-            int linksSize = links.size();
-            System.out.println("Number of links present are: " + linksSize);
-
-            for(int i = 0; i < linksSize; i++)
+            try
             {
-                WebElement element =links.get(i);
-                String url =element.getAttribute("href");
-                verifyLinks(url);
+                List<WebElement> links = driver.findElements(By.tagName("a"));
+                int linksSize = links.size();
+                System.out.println("Number of links present are: " + linksSize);
+
+                for(int i = 0; i < linksSize; i++)
+                {
+                    WebElement element =links.get(i);
+                    String url =element.getAttribute("href");
+                    verifyLinks(url);
+                }
+            } catch (IOException e)
+            {
+                e.printStackTrace();
             }
         }
 
         private void verifyLinks(String url) throws IOException
         {
-            URL verifyUrl = new URL(url);
-            HttpURLConnection httpURLConnection = (HttpURLConnection)verifyUrl.openConnection();
-            httpURLConnection.setConnectTimeout(4000);
-            httpURLConnection.connect();
+            try
+            {
+                URL verifyUrl = new URL(url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)verifyUrl.openConnection();
+                httpURLConnection.setConnectTimeout(4000);
+                httpURLConnection.connect();
 
-            if(httpURLConnection.getResponseCode() < 400)
+                if(httpURLConnection.getResponseCode() < 400)
+                {
+                    Assert.assertTrue(true);
+                    System.out.println("No broken links available");
+                }
+                else
+                {
+                    throw new HttpResponseException(httpURLConnection.getResponseCode(), "Broken link");
+                }
+            } catch (IOException e)
             {
-                Assert.assertTrue(true);
-                System.out.println("No broken links available");
-            }
-            else
-            {
-                throw new HttpResponseException(httpURLConnection.getResponseCode(), "Broken link");
+                e.printStackTrace();
             }
         }
 
         @Then("User should be redirected to the correct page")
         public void userShouldBeRedirectedToTheCorrectPage()
         {
-            dashboardPage.clickAdminLink();
-            Assert.assertEquals(adminUrl,driver.getCurrentUrl());
+            try
+            {
+                dashboardPage.clickAdminLink();
+                Assert.assertEquals(adminUrl,driver.getCurrentUrl());
 
-            dashboardPage.clickPimLink();
-            Assert.assertEquals(pimUrl, driver.getCurrentUrl());
+                dashboardPage.clickPimLink();
+                Assert.assertEquals(pimUrl, driver.getCurrentUrl());
 
-            dashboardPage.clickLeaveLink();
-            Assert.assertEquals(leaveUrl, driver.getCurrentUrl());
+                dashboardPage.clickLeaveLink();
+                Assert.assertEquals(leaveUrl, driver.getCurrentUrl());
 
-            dashboardPage.clickTimelink();
-            Assert.assertEquals(timeUrl, driver.getCurrentUrl());
+                dashboardPage.clickTimelink();
+                Assert.assertEquals(timeUrl, driver.getCurrentUrl());
 
-            dashboardPage.clickRecruitmentLink();
-            System.out.println(driver.getCurrentUrl());
-            Assert.assertEquals(recruitmentUrl, driver.getCurrentUrl());
+                dashboardPage.clickRecruitmentLink();
+                System.out.println(driver.getCurrentUrl());
+                Assert.assertEquals(recruitmentUrl, driver.getCurrentUrl());
 
-            dashboardPage.clickMyInfoLink();
-            Assert.assertEquals(myInfoUrl, driver.getCurrentUrl());
+                dashboardPage.clickMyInfoLink();
+                Assert.assertEquals(myInfoUrl, driver.getCurrentUrl());
 
-            dashboardPage.clickPerformanceLink();
-            Assert.assertEquals(performanceUrl, driver.getCurrentUrl());
+                dashboardPage.clickPerformanceLink();
+                Assert.assertEquals(performanceUrl, driver.getCurrentUrl());
 
-            dashboardPage.clickDashboardLink();
-            Assert.assertEquals(dashboardUrl, driver.getCurrentUrl());
+                dashboardPage.clickDashboardLink();
+                Assert.assertEquals(dashboardUrl, driver.getCurrentUrl());
 
-            dashboardPage.clickDirectoryLink();
-            Assert.assertEquals(directoryUrl, driver.getCurrentUrl());
+                dashboardPage.clickDirectoryLink();
+                Assert.assertEquals(directoryUrl, driver.getCurrentUrl());
 
-            dashboardPage.clickMaintenanceLink();
-            Assert.assertEquals(maintenanceUrl, driver.getCurrentUrl());
-            driver.navigate().to(dashboardUrl);
+                dashboardPage.clickMaintenanceLink();
+                Assert.assertEquals(maintenanceUrl, driver.getCurrentUrl());
+                driver.navigate().to(dashboardUrl);
 
-            dashboardPage.clickBuzzLink();
-            Assert.assertEquals(buzzUrl, driver.getCurrentUrl());
+                dashboardPage.clickBuzzLink();
+                Assert.assertEquals(buzzUrl, driver.getCurrentUrl());
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
         }
     }
