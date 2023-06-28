2f65d3e0898e9808b35f40b257b8a10905c89adb
Thapelo-Sean <thapelomatji12@gmail.com>
2023-06-28 13:49:22 +0200

    Modified TestUrls class

--- src/test/java/OrangeHRM/TestUrls.java
+++ src/test/java/OrangeHRM/TestUrls.java
@@ -1,6 +1,19 @@
 package OrangeHRM;
 
-public class TestUrls {
+public class TestUrls
+{
 
-
+    public final String baseUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+    public final String dashboardUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index";
+    public final String sendPasswordResetUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/sendPasswordReset";
+    public final String adminUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewSystemUsers";
+    public final String pimUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewEmployeeList";
+    public final String leaveUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/leave/viewLeaveList";
+    public final String timeUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/time/viewEmployeeTimesheet";
+    public final String recruitmentUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/recruitment/viewCandidates";
+    public final String myInfoUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPersonalDetails/empNumber/7";
+    public final String performanceUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/performance/searchEvaluatePerformanceReview";
+    public final String directoryUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/directory/viewDirectory";
+    public final String maintenanceUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/maintenance/purgeEmployee";
+    public final String buzzUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/buzz/viewBuzz";
 }
