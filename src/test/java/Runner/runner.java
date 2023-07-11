67ec78add25262327550e351ad1be0ece10d195a
Thapelo-Sean <thapelomatji12@gmail.com>
2023-07-11 11:08:01 +0200

    Modified runner class

--- src/test/java/Runner/runner.java
+++ src/test/java/Runner/runner.java
@@ -4,12 +4,10 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "Features",
 monochrome = true,
 glue = {"StepDefinition"},
-
         plugin = {"pretty", "html:target/cucumber-reports/TestReports.html"}) // Output format and location of the test report)
 public class runner
 {
